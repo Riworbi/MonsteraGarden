370371f13b4a4985dd83061ec93b76d2e7f25981
Krystian Maniewski <riworbix@gmail.com>
2024-06-12 23:58:40 +0200

    cache approach

---
+++ src/app/model/plant.ts
@@ -0,0 +1,11 @@
+export class Plant{
+
+    constructor(
+    public id: number,
+    public slug: string,
+    public scientific_name: string,
+    public image_url: string,
+    public family: string
+    ) {}
+    
+}
--- src/app/species/species.component.ts
+++ src/app/species/species.component.ts
@@ -1,20 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { map, Observable, of, Subscription } from 'rxjs';
 import { ControllerService } from '../controller/controller.service';
+import { Plant } from '../model/plant';
+import { CacheService } from '../cache/cache.service';
 
 @Component({
   selector: 'app-species',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './species.component.html',
-  styleUrl: './species.component.scss'
+  styleUrl: './species.component.scss',
 })
 export class SpeciesComponent implements OnInit {
-  
+  constructor(public controller: ControllerService, cacheService: CacheService) {}
 
-  constructor(public controller : ControllerService) {}
+  plants: Observable<Plant[]> = of();
+  sizeOfArray: number = 0;
+  cacheSubscription: Subscription;
 
-  ngOnInit(): void {
-    this.controller.getTrefle();
+
+  getHalfLength(): number {
+    var result: number;
+    this.plants
+      .pipe(
+        map((array) => {
+          return array.length / 2;
+        })
+      )
+      .subscribe((item) => {
+        this.sizeOfArray = item;
+      });
+    console.log(this.sizeOfArray);
+    return this.sizeOfArray;
   }
 
+  ngOnInit(): void {
+    this.cache
+    this.plants = this.controller.getTrefle();
+    this.getHalfLength();
+  }
 }
